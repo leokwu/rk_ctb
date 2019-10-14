1cd37945d484c40cf37419e68da09ee8454b0feb
leok <leok.wu@rock-chips.com>
2019-10-14 16:51:57 +0800

    1.update solo build host script Signed-off-by: leok.wu <leok.wu@rock-chips.com>

--- host/build.sh
+++ host/build.sh
@@ -1,2 +1,2 @@
 rm -rf test
-g++  test_unique_device2.cpp -o test -I/usr/include/libusb-1.0/  -L/usr/lib/x86_64-linux-gnu/  -lusb-1.0
+g++  test_unique_device2.cpp ../utils/slog.cpp -o test -I/usr/include/libusb-1.0/ -I../utils/  -L/usr/lib/x86_64-linux-gnu/  -lusb-1.0
