89d69dfed5f223dc32c3c480afaa3e2f686d7931
leok.wu <leok.wu@rock-chips.com>
2019-10-14 04:58:49 -0400

    1.fix write buffer null error Signed-off-by: leok.wu <leok.wu@rock-chips.com>

--- device/ctb_device_demo.cpp
+++ device/ctb_device_demo.cpp
@@ -6,11 +6,11 @@ int main(int argc, char *argv[])
 {
 	int i, ret, size;
 
-	ctb_device.mBulkReadBuffer[0] = 0x55;
-	ctb_device.mBulkReadBuffer[4] = 0x18;
-	ctb_device.mBulkReadBuffer[8] = 0x2;
-	ctb_device.mBulkReadBuffer[12] = 0xbb;
-	ctb_device.mBulkReadBuffer[20] = 0x22;
+	ctb_device.mBulkWriteBuffer[0] = 0x55;
+	ctb_device.mBulkWriteBuffer[4] = 0x18;
+	ctb_device.mBulkWriteBuffer[8] = 0x2;
+	ctb_device.mBulkWriteBuffer[12] = 0xbb;
+	ctb_device.mBulkWriteBuffer[20] = 0x22;
 
 	unsigned length;
 
