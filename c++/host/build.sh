f94fb30a47083847085d483dd792f41a1a22e720
leok <leok.wu@rock-chips.com>
2019-10-16 17:20:56 +0800

    1.adjust build script Signed-off-by: leok.wu <leok.wu@rock-chips.com>

--- c++/host/build.sh
+++ c++/host/build.sh
@@ -1,2 +1,2 @@
-rm -rf test
-g++  test_unique_device2.cpp ../utils/slog.cpp -o test -I/usr/include/libusb-1.0/ -I../utils/  -L/usr/lib/x86_64-linux-gnu/  -lusb-1.0
+rm -rf ctb_host
+g++  ctb_host.cpp ../utils/slog.cpp -o ctb_host -I/usr/include/libusb-1.0/ -I../utils/  -L/usr/lib/x86_64-linux-gnu/  -lusb-1.0
