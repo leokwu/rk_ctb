7049ef7117d4619974d94f68cecc6b53daf19cdc
leok <leok.wu@rock-chips.com>
2019-10-16 17:20:01 +0800

    1.add python path Signed-off-by: leok.wu <leok.wu@rock-chips.com>

---
+++ python/device/utils.py
@@ -0,0 +1,25 @@
+"""1808 ctb devide control"""
+
+import select
+import os, sys
+
+
+class CtbDevice():
+    def __init__(self):
+        print("init")
+        rfd = os.open("./read.log", os.O_RDWR | os.O_NONBLOCK)
+        if rfd < 0:
+            print("open read node failed")
+            sys.exit(-1)
+        wfd = os.open("./write.log", os.O_RDWR | os.O_NONBLOCK)
+        if wfd < 0:
+            print("open read node failed")
+            sys.exit(-1)
+
+        print("init succeed")
+
+    def ctb_read(self):
+        print("read")
+
+    def ctb_write(self):
+        print("write")
