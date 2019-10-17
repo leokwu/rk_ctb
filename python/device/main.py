8943c526f082e0c1ddaef2ecf66cf4b1b0e155cf
leok.wu <leok.wu@rock-chips.com>
2019-10-17 03:06:53 -0400

    1.update write interface decide if you've written it all Signed-off-by: leok.wu <leok.wu@rock-chips.com>

--- python/device/main.py
+++ python/device/main.py
@@ -3,5 +3,6 @@ from utils import *
 if __name__ == '__main__':
     print("main")
     ctb_device = CtbDevice()
-    len = ctb_device.ctb_write("1111")
-    print("len: \n", len)
+    length = ctb_device.ctb_write(('1111').encode(), len(('1111').encode()))
+    print("write length: \n", length)
+    ctb_device.ctb_release()
--- python/device/utils.py
+++ python/device/utils.py
@@ -12,41 +12,45 @@ class CtbDevice():
 
         logger.debug("init")
 
-        rfd = os.open("/dev/usb-ffs/ctb/ep1", os.O_RDWR | os.O_NONBLOCK)
-        if rfd < 0:
+        self.rfd = os.open("/dev/usb-ffs/ctb/ep1", os.O_RDWR | os.O_NONBLOCK)
+        if self.rfd < 0:
             logger.error("open read node failed")
             sys.exit(-1)
-        wfd = os.open("/dev/usb-ffs/ctb/ep2", os.O_RDWR | os.O_NONBLOCK)
-        if wfd < 0:
+        self.wfd = os.open("/dev/usb-ffs/ctb/ep2", os.O_RDWR | os.O_NONBLOCK)
+        if self.wfd < 0:
             logger.error("open read node failed")
             sys.exit(-1)
-
+        '''
         try:
             epoll_rfd = select.epoll()
-            epoll_rfd.register(rfd, select.EPOLLIN)
+            epoll_rfd.register(self.rfd, select.EPOLLIN)
         except select.error as error_rmsg:
-            logger.error("error_rmsg rfd: \n", error_rmsg)
+            logger.error("error_rmsg rfd: %s" % (error_rmsg))
 
         try:
             epoll_wfd = select.epoll()
-            epoll_wfd.register(wfd, select.EPOLLOUT)
+            epoll_wfd.register(self.wfd, select.EPOLLOUT)
         except select.error as error_wmsg:
-            logger.error("error_wmsg rfd: \n", error_wmsg)
-
+            logger.error("error_wmsg wfd: %s" % (error_wmsg))
+        '''
         logger.debug("init succeed")
 
-    def ctb_read(self, len):
+    def ctb_read(self, length):
         logger.debug("read")
-        read_data = os.read(self.rfd, len)
-        logger.debug("read_data: \n", read_data)
+        data = os.read(self.rfd, length)
+        logger.debug("read data: %s" % (data))
 
-        return read_data
+        return data
 
-    def ctb_write(self, write_data):
+    def ctb_write(self, data, length):
         logger.debug("write")
-        write_len = os.write(self.wfd, write_data)
-        logger.debug("the numbler of bytes: \n", write_len)
-        return write_len
+        length_count = 0
+        while True:
+            length_count += os.write(self.wfd, data)
+            if length_count == length:
+                break
+        logger.debug("the numbler of bytes: %s" % (length_count))
+        return length_count
 
     def ctb_release(self):
         logger.debug("release resource")
