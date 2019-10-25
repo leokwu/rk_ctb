355bcf56a861beec172b96e32938796b7cb5dfa5
leok.wu <leok.wu@rock-chips.com>
2019-10-24 21:08:36 -0400

    1.delete useless py file Signed-off-by: leok.wu <leok.wu@rock-chips.com>

--- python/device/mobilenet-ssd/main.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-
-if __name__ == '__main__':
-    ctb_device = CtbDevice()
-    logger.debug("main ")
-    length = ctb_device.ctb_write(('1111').encode(), len(('1111').encode()))
-    logger.debug("write length: %s" % (length))
-    ctb_device.ctb_release()
